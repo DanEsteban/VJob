1f75385754dbef9b5bc02a13b23d1188c3772574
Isaac Velasco <isaacvelasco2000@gmail.com>
2024-01-26 19:44:57 -0500

    Arreglado Suma Secuencial y si es CI que se ponga factura por defecto

--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -915,8 +915,8 @@
     function buscarPorRuc(object){
 
         let ruc = object.value;
-        
-        if(ruc){
+        console.log(ruc);
+        if(ruc !== '9999999999999'){
             let url = "/invoices/buscarCliente/" + ruc;        
             $.ajax({
                 type: 'GET',
@@ -928,7 +928,6 @@
                     console.log(xhr.error);
                 },
                 success : function(data){
-                    console.log(data.length);
                     if (data.length !== 0) {
                         $('#cliente').val(data[0]['cliente'])
                         $('#direccion').val(data[0]['direccion'])
@@ -936,7 +935,6 @@
                         $('#email').val(data[0]['email'])
                         $('#id_cliente').val(data[0]['id'])
                     } else {
-                        console.log(data);
                         $('#cliente').val('')
                         $('#direccion').val('')
                         $('#telefono').val('')
@@ -944,13 +942,23 @@
                         $('#id_cliente').val('')
                     }
                 }
-            });    
-        }
+            });            
+
+            var nombreABuscar = "Factura";
+            var tabla = $("#modal-tabla tbody tr");
+            tabla.removeClass("table-active");
+
+            tabla.filter(function() {
+                selectDoc(this);
+                return $(this).find("td:eq(1)").text() === nombreABuscar;
+                }).addClass("table-active");
+            }
 
     }
 
     function selectDoc(object){
 
+        console.log(object);
         $('tr').removeClass('table-active');
         $(object).addClass('table-active');
 
